0a7cc928057b6ac2112b63260102f9490f32627c
osmanidik <osmanidikresmi@gmail.com>
2021-03-12 16:32:23 +0000

    App store management page done.

--- src/pages/apps/containers/layout.jsx
+++ src/pages/apps/containers/layout.jsx
@@ -22,6 +22,7 @@ import { Icon } from '@kube-design/components'
 
 import { renderRoutes } from 'utils/router.config'
 import { Nav } from 'components/Layout'
+import RouteButton from 'components/Layout/RouteButton'
 
 import styles from './layout.scss'
 
@@ -34,21 +35,33 @@ export default class AppsLayout extends Component {
     return (
       <div className="ks-page-body">
         <div className="ks-page-side">
-          <div className={styles.titleWrapper}>
-            <div className={styles.icon}>
-              <Icon name="openpitrix" size={40} type="light" />
-            </div>
-            <div className={styles.text}>
-              <div className="h6">{t('App Store Management')}</div>
-              <p>{t('Platform App Store Management')}</p>
-            </div>
-          </div>
-          <Nav
-            className="ks-page-nav"
-            navs={globals.app.getManageAppNavs()}
-            location={location}
-            match={match}
-          />
+          <RouteButton icon="dashboard" title="Workbench" link="dashboard" />
+          {globals.app.getGlobalNavs().map(nav =>
+            nav.name === 'app-store-management' ? (
+              <div>
+                <div className={styles.titleWrapper}>
+                  <div className={styles.icon}>
+                    <Icon name="openpitrix" size={40} type="light" />
+                  </div>
+                  <div className={styles.text}>
+                    <div className="h6">{t('App Store Management')}</div>
+                    <p>{t('Platform App Store Management')}</p>
+                  </div>
+                </div>
+                <Nav
+                  className="ks-page-nav"
+                  navs={globals.app.getManageAppNavs()}
+                  location={location}
+                  match={match}
+                />
+              </div>
+            ) : (
+              <RouteButton icon={nav.icon} title={nav.title} link={nav.name} />
+            )
+          )}
+          {globals.app.enableAppStore && (
+            <RouteButton icon="appcenter" title="App Store" link="apps" />
+          )}
         </div>
         <div className="ks-page-main">{renderRoutes(route.routes)}</div>
       </div>
